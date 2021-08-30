481e32e8042a00ab7633e88bc32fdd624c631a1b
aragon12 <sm95007@gmail.com>
2021-08-30 16:08:15 +0530

    bankapp: add App Drawer
    App Drawer with toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import AppDrawer from "./AppDrawer";
 import Login from "./Login";
 
 function App() {
-    return(
-      <div>
-        <Login />
-      </div>
-    )
+  return(
+    <div>
+      <AppDrawer />
+      <Login />
+    </div>
+  )
 }
 
 export default App;
