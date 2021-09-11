2f8c6de1e97b9dcd151dd79be4ac0908fcb1a273
aragon12 <sm95007@gmail.com>
2021-09-11 20:16:51 +0530

    bankapp: add testing area
    will be removed once i am done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./Home";
 import Login from "./Login";
 import AppDrawer from "./AppDrawer";
+import Test from "./Test";
 import { Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
+      <Route exact path='/test' component={Test} />
     </Switch>
     </AppDrawer>
   )
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+  return(
+    <div>
+      hello
+    </div>
+  )
+};
+
+export default Test;
